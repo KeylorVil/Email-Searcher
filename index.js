e6d2c6c8a7252898adb5f14dc138bf3c6e224de2
Keylor Villalobos M <keylor.villalobosm@gmail.com>
2020-07-25 15:09:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use('/src', express.static(__dirname + '/src'));
