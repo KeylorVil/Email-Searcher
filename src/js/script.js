334a5376e5ee85970a7b57b935bb7335409ea933
Keylor Villalobos M <keylor.villalobosm@gmail.com>
2020-07-26 22:05:31 -0600

    search css + validations

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,18 @@
 document.querySelector('#searchBtn').addEventListener("click", function () {
     let email = document.querySelector('#emailInput').value;
-    if (email !== '') {
+    if (email !== '' && validateEmail(email)) {
+        document.querySelector('#emailInput').classList.remove('error');
+        document.querySelector('#labelInput').style.visibility = 'visible';
+        document.querySelector('#errorLabel').style.visibility = 'hidden';
         window.location.href = '/result?search=' + email;
+    } else {
+        document.querySelector('#emailInput').classList.add('error');
+        document.querySelector('#labelInput').style.visibility = 'hidden';
+        document.querySelector('#errorLabel').style.visibility = 'visible';
     }
 });
+
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
